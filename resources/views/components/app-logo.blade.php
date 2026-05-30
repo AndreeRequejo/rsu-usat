8ff4a32a8914afc41135a877683c4ab3dda02ce2
Andree Requejo <andree09requejo@gmail.com>
2026-05-30 00:18:56 -0500

    refactor: actualizar íconos y logotipos en la aplicación

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -4,13 +4,13 @@
 
 @if($sidebar)
     <flux:sidebar.brand name="RSU - USAT" {{ $attributes }}>
-        <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
+        <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md bg-gray-100 text-accent-foreground">
             <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
         </x-slot>
     </flux:sidebar.brand>
 @else
     <flux:brand name="RSU - USAT" {{ $attributes }}>
-        <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
+    <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md bg-gray-100 text-accent-foreground">
             <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
         </x-slot>
     </flux:brand>
