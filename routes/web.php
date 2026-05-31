028fd32e80fc3ab3301a29a260bd46e85bae5ab5
ArmandoInfanteBernal <infantearmando421@gmail.com>
2026-05-31 12:59:47 -0500

    refactor: inglés b1 usat

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::livewire('vehiculos/modelos', 'vehicles.models.index')->name('vehicles.models.index');
     Route::livewire('vehiculos/tipos', 'vehicles.types.index')->name('vehicles.types.index');
     Route::livewire('personal/tipos', 'personnel.types.index')->name('personnel.types.index');
-    Route::livewire('personal/personal', 'personnel.personnel.index')->name('personnel.personal.index');
+    Route::livewire('personal/personal', 'personnel.personnel.index')->name('personnel.personnel.index');
     Route::livewire('personal/asistencias', 'personnel.attendance.index')->name('personnel.attendance.index');
     Route::livewire('personal/contratos', 'personnel.contracts.index')->name('personnel.contracts.index');
     Route::livewire('personal/vacaciones', 'personnel.vacations.index')->name('personnel.vacations.index');
