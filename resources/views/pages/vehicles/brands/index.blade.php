90c9e2bb46b48d7d76219d875ea9fd357a4b8dc4
ArmandoInfanteBernal <infantearmando421@gmail.com>
2026-05-31 16:10:43 -0500

    refactor: remove redundant filter button

--- resources/views/pages/vehicles/brands/index.blade.php
+++ resources/views/pages/vehicles/brands/index.blade.php
@@ -208,9 +208,6 @@ new class extends Component {
                     class="w-full pl-10 pr-4 py-2.5 border border-[#A5D6A7] rounded-lg bg-white text-sm focus:outline-none focus:ring-2 focus:ring-[#2E8B57]"
                 />
             </div>
-            <button class="px-6 py-2.5 bg-[#2E8B57] text-white text-sm font-medium rounded-lg">
-                {{ __('Filtrar') }}
-            </button>
         </div>
     </div>
 
